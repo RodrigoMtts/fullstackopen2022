255c858ffe9e1ef6344b5f8a1cca8e0d9ff0d127
Rafael da Matta <rafael.matta@csptecnologia.com>
2022-12-05 18:34:44 -0300

    part2, phonebook, step 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      phone: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,7 +18,10 @@ const App = () => {
       return null
     }
     
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({
+      name: newName,
+      phone: newPhone
+    }))
     setNewName('')
   }
 
@@ -23,12 +30,17 @@ const App = () => {
     setNewName(event.target.value)
   } 
 
+  const phoneEventHandler = (event) => {
+      setNewPhone(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={nameEventHandler} value={newName}/>
+          phone: <input onChange={phoneEventHandler} value={newPhone}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,7 +49,7 @@ const App = () => {
       <h2>Numbers</h2>
       {
         persons.map( (person,i) => {
-          return <div key={i}>{person.name}</div>
+          return <div key={i}>{person.name} {person.phone}</div>
         })
       }
     </div>
