8141f2d0020d937555023faa2cf4b279973da756
Rafael da Matta <rafael.matta@csptecnologia.com>
2022-12-05 18:12:07 -0300

    part2, phonebook, step 1.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const nameEventHandler = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  } 
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={nameEventHandler} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map( (person,i) => {
+          return <div key={i}>{person.name}</div>
+        })
+      }
+    </div>
+  )
+}
+
+export default App
