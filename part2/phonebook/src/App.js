f78f2a35077ea9440e2b0d3c4c4f5ddf324b9742
Rafael da Matta <rafael.matta@csptecnologia.com>
2022-12-05 21:39:52 -0300

    part2, phonebook, step 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -17,39 +18,46 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return null
     }
-    
     setPersons(persons.concat({
       name: newName,
-      phone: newPhone
+      number: newNumber,
+      id: persons[persons.length - 1].id + 1
     }))
     setNewName('')
   }
 
   const nameEventHandler = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   } 
 
   const phoneEventHandler = (event) => {
-      setNewPhone(event.target.value)
+      setNewNumber(event.target.value)
+  }
+
+  const filterEventHandler = (event) => {
+    setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={filterEventHandler} value={filter}/>
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={nameEventHandler} value={newName}/>
-          phone: <input onChange={phoneEventHandler} value={newPhone}/>
+          phone: <input onChange={phoneEventHandler} value={newNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {console.log("Filter", filter)}
       {
-        persons.map( (person,i) => {
-          return <div key={i}>{person.name} {person.phone}</div>
+        persons.filter( person => person.name.toLocaleLowerCase().includes(filter)).map( (person,i) => {
+          return <div key={person.id}>{person.name} {person.number}</div>
         })
       }
     </div>
